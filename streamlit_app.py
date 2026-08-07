adeb985c533b4cb23b130908591fe416db692b5b
rpdoody <rdoody@cox.net>
2026-08-07 19:46:29 -0400

    Refactor Streamlit app for navigation and pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
-st.title("RP Rocket Lab")
-st.write(
-    "Moneyline, spreads, totals"
-)
-sidebar
+page1 = st.Page("home.py", title="Home", icon="⚾")
+page2 = st.Page("pages/kbo_sim.py", title="KBO Simulation", icon="📊")
+page3 = st.Page("pages/mlb_sim.py", title="MLB Simulation", icon="📈")
+
+pg = st.navigation([page1, page2, page3])
+pg.run()
